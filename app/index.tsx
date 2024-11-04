33951c55d865f4920533eb7cd9a23563e1679b66
Tural Hajiyev <thajiyev9@outlook.com>
2024-11-04 21:57:13 +0100

    Setup prettier and eslint

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, Platform, StatusBar } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View
+        style={{
+          backgroundColor: 'green',
+          padding: 16,
+          marginTop: isAndroid ? StatusBar.currentHeight : 0,
+        }}
+      >
+        <Text>Search</Text>
+      </View>
+      <View style={{ flex: 9, backgroundColor: 'red', padding: 16 }}>
+        <Text>List</Text>
+      </View>
     </View>
   );
 }
