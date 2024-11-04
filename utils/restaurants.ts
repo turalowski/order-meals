3b1271053064c3a900da6e68826c90bd88b97a4e
Tural Hajiyev <thajiyev9@outlook.com>
2024-11-04 22:47:06 +0100

    Moved restaurants data to a new file

---
+++ utils/restaurants.ts
@@ -0,0 +1,35 @@
+export const restaurantData = [
+    {
+      name: "Sample Restaurant",
+      icon: "",
+      photos: [
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.foodiesfeed.com%2Fwp-content%2Fuploads%2F2017%2F07%2Fshakshuka-1.jpg&f=1&nofb=1&ipt=9e9f003e3ecca4ad82c964263a9183e63334d72272d22c873919421c07e5dc59&ipo=images',
+      ],
+      address: "123 Main Street, City",
+      isOpenNow: true,
+      rating: 4.5,
+      isClosedTemporary: false
+    },
+    {
+      name: "Italian Bistro",
+      icon: "",
+      photos: [
+        'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made.jpg',
+      ],
+      address: "456 Pasta Lane, Downtown",
+      isOpenNow: true,
+      rating: 4.7,
+      isClosedTemporary: false
+    },
+    {
+      name: "Sushi Express",
+      icon: "",
+      photos: [
+        'https://www.foodiesfeed.com/wp-content/uploads/2023/08/sushi-roll-macro.jpg'
+      ],
+      address: "789 Ocean Drive, Seaside",
+      isOpenNow: false,
+      rating: 4.3,
+      isClosedTemporary: true
+    }
+  ];
